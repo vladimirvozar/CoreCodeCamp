4ff06140a1cd47bbe01a95810118657b0dbf74fe
Vladimir Vozar <vladimirvozar@gmail.com>
2020-04-20 12:50:39 +0200

    Created entities

---
+++ CoreCodeCamp/Data/Entities/Camp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Data.Entities
+{
+    public class Camp
+    {
+        public int CampId { get; set; }
+        public string Name { get; set; }
+        public string Moniker { get; set; }
+        public Location Location { get; set; }
+        public DateTime EventDate { get; set; } = DateTime.MinValue;
+        public int Length { get; set; }
+        public ICollection<Task> Talks { get; set; }
+    }
+}
