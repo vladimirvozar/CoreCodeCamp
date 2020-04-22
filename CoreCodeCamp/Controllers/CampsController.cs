7c16cdc55143c01ed262ae8a2fbade1454056c87
Vladimir Vozar <vladimirvozar@gmail.com>
2020-04-22 22:09:38 +0200

    Implemented creating, updating and deleting a camp

--- CoreCodeCamp/Controllers/CampsController.cs
+++ CoreCodeCamp/Controllers/CampsController.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using CoreCodeCamp.Data;
+using CoreCodeCamp.Data.Entities;
 using CoreCodeCamp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +13,18 @@ using System.Threading.Tasks;
 namespace CoreCodeCamp.Controllers
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class CampsController : ControllerBase
     {
         private readonly ICampRepository _repository;
         private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
 
-        public CampsController(ICampRepository repository, IMapper mapper)
+        public CampsController(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator)
         {
             _repository = repository;
             _mapper = mapper;
+            _linkGenerator = linkGenerator;
         }
 
         [HttpGet]
@@ -69,6 +74,89 @@ namespace CoreCodeCamp.Controllers
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
             }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CampModel>> Post(CampModel model)
+        {
+            try
+            {
+                var existingCamp = await _repository.GetCampAsync(model.Moniker);
+                if (existingCamp != null)
+                {
+                    return BadRequest("Moniker in use");
+                }
+
+                var location = _linkGenerator.GetPathByAction("Get", "Camps", new { moniker = model.Moniker });
+
+                if(string.IsNullOrWhiteSpace(location))
+                {
+                    return BadRequest("Could not use current moniker");
+                }
+
+                // Create a new camp
+                var camp = _mapper.Map<Camp>(model);
+                _repository.Add(camp);
+
+                if(await _repository.SaveChangesAsync())
+                {
+                    return Created(location, _mapper.Map<CampModel>(camp));
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("{moniker}")]
+        public async Task<ActionResult<CampModel>> Put(string moniker, CampModel model)
+        {
+            try
+            {
+                var oldCamp = await _repository.GetCampAsync(moniker);
+
+                if (oldCamp == null) return NotFound($"Could not find camp with moniker of {moniker}");
+
+                _mapper.Map(model, oldCamp);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return _mapper.Map<CampModel>(oldCamp);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("{moniker}")]
+        public async Task<IActionResult> Delete(string moniker)
+        {
+            try
+            {
+                var oldCamp = await _repository.GetCampAsync(moniker);
+
+                if (oldCamp == null) return NotFound($"Could not find camp with moniker of {moniker}");
+
+                _repository.Delete(oldCamp);
+
+                if(await _repository.SaveChangesAsync())
+                {
+                    return Ok();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+            return BadRequest("Failed to delete the camp");
         }
 
     }
--- CoreCodeCamp/Data/CampProfile.cs
+++ CoreCodeCamp/Data/CampProfile.cs
@@ -20,7 +20,8 @@ namespace CoreCodeCamp.Data
                 .ForMember(c => c.CityTown, o => o.MapFrom(m => m.Location.CityTown))
                 .ForMember(c => c.StateProvince, o => o.MapFrom(m => m.Location.StateProvince))
                 .ForMember(c => c.PostalCode, o => o.MapFrom(m => m.Location.PostalCode))
-                .ForMember(c => c.Country, o => o.MapFrom(m => m.Location.Country));
+                .ForMember(c => c.Country, o => o.MapFrom(m => m.Location.Country))
+                .ReverseMap();
 
             this.CreateMap<Talk, TalkModel>();
 
